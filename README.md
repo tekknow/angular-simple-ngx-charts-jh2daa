16c42e3ec4feea4870b81c21e260100e3b21dee7
tekknow <greg@tekknow.net>
2023-08-30 09:42:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-simple-ngx-charts-jh2daa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-simple-ngx-charts-jh2daa)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, NgxChartsModule, BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
